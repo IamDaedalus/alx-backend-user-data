63da56b6fb0db5e3dc632225e3dac6c47a24f86d
Daedalus <manny.is.daedalus@pm.me>
2024-04-16 05:59:31 +0000

    tasks 0 t 5

---
+++ 0x01-Basic_authentication/README.md
@@ -0,0 +1,1 @@
+Simple authentication via Basic Auth
---
+++ 0x01-Basic_authentication/SimpleAPI/api/v1/auth/auth.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+""" auth class and API authentication """
+
+
+from typing import List, TypeVar
+from flask import request
+
+
+class Auth:
+    """ authentication class """
+    def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        """ check if the user request requires authentication """
+        if path is None or len(excluded_paths) == 0 or excluded_paths is None:
+            return True
+        if path[-1] != '/':
+            path = path + '/'
+
+        return not (path in excluded_paths)
+
+    def authorization_header(self, request=None) -> str:
+        """ authorization header method """
+        if request is None or request.headers.get('Authorization') is None:
+            return None
+        return request.headers.get('Authorization')
+
+    def current_user(self, request=None) -> TypeVar('User'):
+        """ defines the current user """
+        return None
